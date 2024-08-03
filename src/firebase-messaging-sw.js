467f2b96488aee0481b9996757c16f8290bf939d
alexjamesmx <sg.alex32@gmail.com>
2024-08-03 03:02:44 -0600

    env firebase messagging 3

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,7 +1,10 @@
 // src/firebase-messaging-sw.template.js
-
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js"
+);
 
 firebase.initializeApp({
   apiKey: "%%REACT_APP_FIREBASE_API_KEY%%",
