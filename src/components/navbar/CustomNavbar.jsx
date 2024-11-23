a0241b756c6c279c0d5ac900cd1753fc7b74c0e8
alexjamesmx <sg.alex32@gmail.com>
2024-11-23 02:08:01 -0600

    test22

--- src/components/navbar/CustomNavbar.jsx
+++ src/components/navbar/CustomNavbar.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 // import { Navbar, IconButton } from "@material-tailwind/react";
 // import { Link } from "react-router-dom";
 import { UserContext } from "../../customHooks/UserContext";
 // import Logo from "../../assets/logos/logo40.svg";
 // import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-// import { IconButton, Navbar } from "@material-tailwind/react";
+import { IconButton, Navbar } from "@material-tailwind/react";
+import { useContext } from "react";
 // import { Link } from "react-router-dom";
 
 function CustomNavbar({ children }) {
@@ -24,14 +25,14 @@ function CustomNavbar({ children }) {
   // const isActive = (path) => {
   //   return location.pathname === path;
   // };
-  // const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = useState(false);
 
-  // useEffect(() => {
-  //   window.addEventListener(
-  //     "resize",
-  //     () => window.innerWidth >= 960 && setOpenNav(false)
-  //   );
-  // }, []);
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
 
   // const navList = (
   //   <ul className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 w-full py-">
@@ -88,6 +89,63 @@ function CustomNavbar({ children }) {
 
   return (
     <div>
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="text-blue-gray-900 mx-14 flex items-center justify-between">
+          <div className="mr-4 flex cursor-pointer items-center gap-2 py-1.5 font-medium">
+            {/* <Link to="/">
+              <img src={Logo} alt="Logo" width="40" height="40" />
+            </Link> */}
+          </div>
+          <div className="flex items-center gap-4">
+            {/* <div className="hidden lg:block">{navList}</div> */}
+
+            <IconButton
+              variant="text"
+              className="ml-auto size-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              // onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="size-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+
+        {/* <Collapse
+          open={openNav}
+          className="flex justify-center flex-col items-center w-full"
+        >
+          {navList}
+        </Collapse> */}
+      </Navbar>
       <div className="h-[calc(100vh-6em)] p-8">{children}</div>
     </div>
   );
