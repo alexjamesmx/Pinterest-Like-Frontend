fa40e794c91071d781b4f0a010b322fc01d7f8f3
alexjamesmx <sg.alex32@gmail.com>
2024-11-23 01:53:40 -0600

    test16

--- src/components/navbar/CustomNavbar.jsx
+++ src/components/navbar/CustomNavbar.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect } from "react";
-import { Navbar, Collapse, IconButton } from "@material-tailwind/react";
-import { Link, useLocation } from "react-router-dom";
+import { Navbar, IconButton } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 import { UserContext } from "../../customHooks/UserContext";
 // import Logo from "../../assets/logos/logo40.svg";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function CustomNavbar({ children }) {
@@ -11,17 +11,17 @@ function CustomNavbar({ children }) {
   console.log("CustomNavbar user:", user);
   console.log("CustomNavbar logout function:", logout);
 
-  const location = useLocation();
-  const navigate = useNavigate();
+  // const location = useLocation();
+  // const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
+  // const handleLogout = () => {
+  //   logout();
+  //   navigate("/login");
+  // };
 
-  const isActive = (path) => {
-    return location.pathname === path;
-  };
+  // const isActive = (path) => {
+  //   return location.pathname === path;
+  // };
   const [openNav, setOpenNav] = React.useState(false);
 
   useEffect(() => {
@@ -31,75 +31,74 @@ function CustomNavbar({ children }) {
     );
   }, []);
 
-  const navList = (
-    <ul className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 w-full py-">
-      {user && (
-        <>
-          <Link
-            to="/profile"
-            className="nav-link-custom self-center items-center"
-          >
-            <img
-              src={user?.photoURL || "../../assets/user-default-120.webp"}
-              alt="foto de perfil"
-              width="50"
-              height="50"
-              className={`rounded-full cursor-pointer align-center ${
-                isActive("/profile") ? "ring-4 ring-deep-orange-900" : ""
-              }`}
-            />
-          </Link>
-        </>
-      )}
+  // const navList = (
+  //   <ul className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 w-full py-">
+  //     {user && (
+  //       <>
+  //         <Link
+  //           to="/profile"
+  //           className="nav-link-custom self-center items-center"
+  //         >
+  //           <img
+  //             src={user?.photoURL || "../../assets/user-default-120.webp"}
+  //             alt="foto de perfil"
+  //             width="50"
+  //             height="50"
+  //             className={`align-center cursor-pointer rounded-full ${
+  //               isActive("/profile") ? "ring-deep-orange-900 ring-4" : ""
+  //             }`}
+  //           />
+  //         </Link>
+  //       </>
+  //     )}
 
-      <Link
-        to="/"
-        className={`p-1 font-normal items-center cursor-pointer text-black text-center  ${
-          isActive("/") ? "bg-deep-orange-900 text-white rounded px-4" : ""
-        }`}
-      >
-        Home
-      </Link>
-      {!user ? (
-        <Link
-          to="/login"
-          className={`p-1 font-normal items-center cursor-pointer text-black text-center w-full ${
-            isActive("/login")
-              ? "bg-deep-orange-900 text-white rounded px-4"
-              : ""
-          }`}
-        >
-          Log In
-        </Link>
-      ) : (
-        <Link
-          onClick={handleLogout}
-          className={`p-1 font-normal items-center cursor-pointer text-black  text-center ${
-            isActive("/login") ? "bg-blue-gray-900 text-white rounded px-4" : ""
-          }`}
-        >
-          Logout
-        </Link>
-      )}
-    </ul>
-  );
+  //     <Link
+  //       to="/"
+  //       className={`cursor-pointer items-center p-1 text-center font-normal text-black  ${
+  //         isActive("/") ? "bg-deep-orange-900 text-white rounded px-4" : ""
+  //       }`}
+  //     >
+  //       Home
+  //     </Link>
+  //     {!user ? (
+  //       <Link
+  //         to="/login"
+  //         className={`w-full cursor-pointer items-center p-1 text-center font-normal text-black ${
+  //           isActive("/login")
+  //             ? "bg-deep-orange-900 text-white rounded px-4"
+  //             : ""
+  //         }`}
+  //       >
+  //         Log In
+  //       </Link>
+  //     ) : (
+  //       <Link
+  //         onClick={handleLogout}
+  //         className={`cursor-pointer items-center p-1 text-center font-normal  text-black ${
+  //           isActive("/login") ? "bg-blue-gray-900 rounded px-4 text-white" : ""
+  //         }`}
+  //       >
+  //         Logout
+  //       </Link>
+  //     )}
+  //   </ul>
+  // );
 
   return (
     <div>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900 mx-14">
-          <div className="mr-4 cursor-pointer py-1.5 font-medium flex items-center gap-2">
+        <div className="text-blue-gray-900 mx-14 flex items-center justify-between">
+          <div className="mr-4 flex cursor-pointer items-center gap-2 py-1.5 font-medium">
             <Link to="/">
-              {" "}
               {/* <img src={Logo} alt="Logo" width="40" height="40" /> */}
             </Link>
           </div>
           <div className="flex items-center gap-4">
-            <div className="hidden lg:block">{navList}</div>
+            {/* <div className="hidden lg:block">{navList}</div> */}
 
             <IconButton
               variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              className="ml-auto size-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
               ripple={false}
               onClick={() => setOpenNav(!openNav)}
             >
@@ -137,14 +136,14 @@ function CustomNavbar({ children }) {
           </div>
         </div>
 
-        <Collapse
+        {/* <Collapse
           open={openNav}
           className="flex justify-center flex-col items-center w-full"
         >
           {navList}
-        </Collapse>
+        </Collapse> */}
       </Navbar>
-      <div className="p-8 h-[calc(100vh-6em)]">{children}</div>{" "}
+      <div className="h-[calc(100vh-6em)] p-8">{children}</div>{" "}
     </div>
   );
 }
