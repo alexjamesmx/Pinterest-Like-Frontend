d9034105abea3f1d31a41b596e0d3d33e5ea233d
alexjamesmx <sg.alex32@gmail.com>
2024-11-23 02:34:31 -0600

    test27

--- src/components/navbar/CustomNavbar.jsx
+++ src/components/navbar/CustomNavbar.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useContext } from "react";
-import { Navbar, IconButton } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Navbar, Collapse, IconButton } from "@material-tailwind/react";
+import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "../../customHooks/UserContext";
 import Logo from "../../assets/logos/logo40.svg";
 import { useNavigate } from "react-router-dom";
@@ -8,9 +8,6 @@ import PropTypes from "prop-types";
 
 function CustomNavbar({ children }) {
   const { user, logout } = useContext(UserContext);
-  console.log("CustomNavbar user:", user);
-  console.log("CustomNavbar logout function:", logout);
-
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -22,7 +19,7 @@ function CustomNavbar({ children }) {
   const isActive = (path) => {
     return location.pathname === path;
   };
-  const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = React.useState(false);
 
   useEffect(() => {
     window.addEventListener(
@@ -44,8 +41,8 @@ function CustomNavbar({ children }) {
               alt="foto de perfil"
               width="50"
               height="50"
-              className={`align-center cursor-pointer rounded-full ${
-                isActive("/profile") ? "ring-deep-orange-900 ring-4" : ""
+              className={`rounded-full cursor-pointer align-center ${
+                isActive("/profile") ? "ring-4 ring-deep-orange-900" : ""
               }`}
             />
           </Link>
@@ -54,7 +51,7 @@ function CustomNavbar({ children }) {
 
       <Link
         to="/"
-        className={`cursor-pointer items-center p-1 text-center font-normal text-black  ${
+        className={`p-1 font-normal items-center cursor-pointer text-black text-center  ${
           isActive("/") ? "bg-deep-orange-900 text-white rounded px-4" : ""
         }`}
       >
@@ -63,7 +60,7 @@ function CustomNavbar({ children }) {
       {!user ? (
         <Link
           to="/login"
-          className={`w-full cursor-pointer items-center p-1 text-center font-normal text-black ${
+          className={`p-1 font-normal items-center cursor-pointer text-black text-center w-full ${
             isActive("/login")
               ? "bg-deep-orange-900 text-white rounded px-4"
               : ""
@@ -74,8 +71,8 @@ function CustomNavbar({ children }) {
       ) : (
         <Link
           onClick={handleLogout}
-          className={`cursor-pointer items-center p-1 text-center font-normal  text-black ${
-            isActive("/login") ? "bg-blue-gray-900 rounded px-4 text-white" : ""
+          className={`p-1 font-normal items-center cursor-pointer text-black  text-center ${
+            isActive("/login") ? "bg-blue-gray-900 text-white rounded px-4" : ""
           }`}
         >
           Logout
@@ -87,9 +84,10 @@ function CustomNavbar({ children }) {
   return (
     <div>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-        <div className="text-blue-gray-900 mx-14 flex items-center justify-between">
-          <div className="mr-4 flex cursor-pointer items-center gap-2 py-1.5 font-medium">
+        <div className="flex items-center justify-between text-blue-gray-900 mx-14">
+          <div className="mr-4 cursor-pointer py-1.5 font-medium flex items-center gap-2">
             <Link to="/">
+              {" "}
               <img src={Logo} alt="Logo" width="40" height="40" />
             </Link>
           </div>
@@ -98,7 +96,7 @@ function CustomNavbar({ children }) {
 
             <IconButton
               variant="text"
-              className="ml-auto size-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
               ripple={false}
               onClick={() => setOpenNav(!openNav)}
             >
@@ -106,7 +104,7 @@ function CustomNavbar({ children }) {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  className="size-6"
+                  className="h-6 w-6"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                   strokeWidth={2}
@@ -120,7 +118,7 @@ function CustomNavbar({ children }) {
               ) : (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="size-6"
+                  className="h-6 w-6"
                   fill="none"
                   stroke="currentColor"
                   strokeWidth={2}
@@ -143,7 +141,7 @@ function CustomNavbar({ children }) {
           {navList}
         </Collapse>
       </Navbar>
-      <div className="h-[calc(100vh-6em)] p-8">{children}</div>
+      <div className="p-8">{children}</div>
     </div>
   );
 }
